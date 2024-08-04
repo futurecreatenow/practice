2731f87c5eef3c3ac5a64173c2ea57144bb5697f
futurecreatenow <158742450+futurecreatenow@users.noreply.github.com>
2024-08-04 21:15:25 +0900

    Add files via upload

--- re_study.c
+++ re_study.c
@@ -7,12 +7,8 @@
 #define NUM_CHAR_START '0'
 #define NUM_CHAR_END '9'
 
-// // プロトタイプ宣言
-// void practice(void);// 練習用関数
-// void init(void);//文字列配列の初期化
-// void get_moji(void);//一文字だけ標準入力で文字を格納する
-// void get_string(void);//標準入力で文字列を格納する
-// void count_string(void);//文字のカウント
+//全角数字と文字に対応していない。例えば全角の１２３がそれぞれPQRと認識されてしまう
+
 
 //構造体宣言
 typedef struct MOJI{
@@ -22,35 +18,37 @@ typedef struct MOJI{
     int moji_num;
 }STMOJI;
 
+// プロトタイプ宣言
+void practice(void);// 練習用関数
+void set_moji(STMOJI *st);//頻度表配列情報の初期化
+void get_string(STMOJI *st);//標準入力で文字列を格納する
+void count_string(STMOJI *st);//入力した文字の頻度計算
+
+
 // 練習用関数
 void practice(void){
 	printf("START>>%d\n",NUM_CHAR_START);
 	printf("END>>%d\n",NUM_CHAR_END);
 }
 
-//文字列配列の初期化
-void init(STMOJI st){
+//頻度表配列情報の初期化
+void set_moji(STMOJI *st){
+    st->moji_num = 0;
+    st->moji_kind = 0;
 	int i = 0;
 	for(i = 0;i<STRING_LENGTH;i++){
-		st.array[i] = 0;
+		st->array[i] = 0;
 	}
-}
-//一文字だけ標準入力で文字を格納する
-void get_moji(void){
-	char moji;
-	moji = getchar();
-	printf("get moji>>%c\n",moji);
-
 }
 
 //標準入力で文字列を格納する
-void get_string(STMOJI st){
-	while((st.ch = getchar()) != EOF){
-		if ((CHAR_START_S <= st.ch) && (st.ch <= CHAR_END_S) ||
-			(CHAR_START_L <= st.ch) && (st.ch <= CHAR_END_L) ||
-			(NUM_CHAR_START <= st.ch) && (st.ch <= NUM_CHAR_END))
+void get_string(STMOJI *st){
+	while((st->ch = getchar()) != EOF){
+        if ((CHAR_START_S <= st->ch) && (st->ch <= CHAR_END_S) ||
+			(CHAR_START_L <= st->ch) && (st->ch <= CHAR_END_L) ||
+			(NUM_CHAR_START <= st->ch) && (st->ch <= NUM_CHAR_END))
 		{
-			st.array[st.ch]++; 
+			st->array[st->ch]++; 
 		}
 		else{
 			continue;
@@ -59,25 +57,25 @@ void get_string(STMOJI st){
 	}
 }
 
-void count_string(STMOJI st){
+//入力した文字の頻度計算
+void count_string(STMOJI *st){
 	int i = 0;
 	for(i = 0;i < STRING_LENGTH;i++){
-		if(st.array[i] != 0){
-			printf("%c>>>%d\n",i,st.array[i]);
-			st.moji_kind++;
-			st.moji_num += st.array[i];
+		if(st->array[i] != 0){
+			printf("%c>>>%d\n",i,st->array[i]);
+			st->moji_kind++;
+			st->moji_num += st->array[i];
 		}
 	}
-	printf("moji_kind>>>%d\n",st.moji_kind);
-	printf("moji_num>>>%d\n",st.moji_num);
+	printf("moji_kind>>>%d\n",st->moji_kind);
+	printf("moji_num>>>%d\n",st->moji_num);
 }
+
 int main(void){
     STMOJI mojifre;
-
-	init(mojifre);
-	get_string(mojifre);
-	// get_moji();
-	//practice();
-	count_string(mojifre);
+    // practice();
+	set_moji(&mojifre);
+	get_string(&mojifre);
+	count_string(&mojifre);
 	return 0;
 }
