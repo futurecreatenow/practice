2ad74f4f6ce367d906251abb49e38ebf595273d0
futurecreatenow <158742450+futurecreatenow@users.noreply.github.com>
2024-12-10 17:54:45 +0900

    Add files via upload

---
+++ input_device/input_device_ds.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include <mydef.h>
+
+#ifndef INPUT_DEVICE_DS_H
+#define INPUT_DEVICE_DS_H
+
+typedef struct input_device_ {
+   FILE* inputFile;
+   boolean eofReached;
+} input_device, *acc_input_device;
+
+#endif
