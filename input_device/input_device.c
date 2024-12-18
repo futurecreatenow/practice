fc245421b955f253c2efa17ce804789f5377701e
futurecreatenow <158742450+futurecreatenow@users.noreply.github.com>
2024-12-18 21:41:47 +0900

    Add files via upload

---
+++ input_device/input_device.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../mydef/mydef.h"
+#include "../heap/heap.h"
+#include "../stm/token.h"
+#include "input_device.h"
+
+/* constructor of input device */
+
+acc_input_device INDCreate(FILE* inputFile) {
+   acc_input_device aid;
+
+   aid = (acc_input_device) HPAlloc(HPCreate(), sizeof(input_device));
+   aid->inputFile = (inputFile == null_FILE) ? stdin : inputFile;
+
+   return aid;
+}
+
+/* destructor of input device */
+
+void INDDispose(acc_input_device aid) { HPFree(HPCreate(), (void *) aid); }
+
+void INDSet_EOF(acc_input_device aid) { aid->eofReached = YES; }
+
+void INDReset_EOF(acc_input_device aid) { aid->eofReached = NO; }
+
+boolean INDEOF_Reached(acc_input_device aid) { return aid->eofReached; }
+
+char INDGet_Char(acc_input_device aid) { return getc(aid->inputFile); }
---
+++ input_device/string_input_device_ds.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "../mydef/mydef.h"
+#include "input_device_ds.h"
+#include "../stm/stm_ds.h"
+
+#ifndef STRING_INPUT_DEVICE_DS_H
+#define STRING_INPUT_DEVICE_DS_H
+
+typedef struct string_input_device_ {
+   acc_input_device accInputDev;
+   acc_stm *stms;
+   int noSTMs;
+} string_input_device, *acc_string_input_device;
+
+#endif
