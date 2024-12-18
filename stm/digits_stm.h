5b5d8e880bbe3d64a414f761fe5e9b30910a220b
futurecreatenow <158742450+futurecreatenow@users.noreply.github.com>
2024-12-18 21:36:26 +0900

    Delete stm directory

--- stm/digits_stm.h
+++
@@ -1,9 +0,0 @@
-#include <stm.h>
-#include <stm_state.h>
-
-typedef acc_stm acc_word_stm;
-
-acc_word_stm DIGCreate(void);
-void DIGInit(void);
-void DIGReset(acc_stm);
-
--- stm/word_stm.h
+++
@@ -1,8 +0,0 @@
-#include <stm.h>
-#include <stm_state.h>
-
-typedef acc_stm acc_word_stm;
-
-acc_word_stm WSCreate(void);
-void WSInit(void);
-void WSReset(acc_stm);
